bdeb0a7c9e21ff9489db6de13a47b228e2e48b53
Achim Grether <a.grether@hotmail.com>
2022-10-02 19:04:57 +0200

    erste versuche mit den permutationen

--- Permutationen/src/ch/ffhs/dua/permutations/Permutations.java
+++ Permutationen/src/ch/ffhs/dua/permutations/Permutations.java
@@ -1,8 +1,16 @@
 package ch.ffhs.dua.permutations;
 
 
-public class Permutations 
+import java.util.Arrays;
+
+
+public class Permutations
 {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(permutations(3)));
+	}
+
 	/**
 	 * Erzeugt ein Array von allen Permutationen von {0,1,2,3,...,n-1}.
 	 * @param n Anzahl Elemente in einer Permutation.
@@ -11,8 +19,31 @@ public class Permutations
 	 */
 	public static int[][] permutations(int n)
 	{
+		if (n == 1) {
+			int [][] perm = new int[2][1];
+			perm[0][0] = 1;
+			perm[1][0] = 1;
+			return perm;
+		}
+		int[][] permutationArray = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			permutations(n - 1);
+
+			if (n % 2 == 1 ){
+				int temp = permutationArray[n][0];
+				permutationArray[n][0] = permutationArray[n][n-1];
+				permutationArray[n][n -1 ] = temp;
+			}
+
+			else {
+				int temp = permutationArray[n][n];
+				permutationArray[n][i] = permutationArray[n][n -1];
+				permutationArray[n][n-1] = temp;
+			}
+		}
 		// TODO
-		return null;
+		return permutationArray;
 	}
 	
 
