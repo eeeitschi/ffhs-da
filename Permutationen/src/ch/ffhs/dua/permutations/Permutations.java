a65c6df3011556a15323244f6ed3e6d7eba8c273
Achim Grether <a.grether@hotmail.com>
2022-10-02 19:21:50 +0200

    erste versuche mit den permutationen

--- Permutationen/src/ch/ffhs/dua/permutations/Permutations.java
+++ Permutationen/src/ch/ffhs/dua/permutations/Permutations.java
@@ -4,47 +4,54 @@ package ch.ffhs.dua.permutations;
 import java.util.Arrays;
 
 
-public class Permutations
-{
+public class Permutations {
 
-	public static void main(String[] args) {
-		System.out.println(Arrays.deepToString(permutations(3)));
-	}
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(permutations(3)));
+    }
 
-	/**
-	 * Erzeugt ein Array von allen Permutationen von {0,1,2,3,...,n-1}.
-	 * @param n Anzahl Elemente in einer Permutation.
-	 * @return  Ein Array von Permutationen; jede Zeile ist eine Permutation von {0,1,2,3,...,n-1}.
-	 * Permutation ist ein Array von Integern.
-	 */
-	public static int[][] permutations(int n)
-	{
-		if (n == 1) {
-			int [][] perm = new int[2][1];
-			perm[0][0] = 1;
-			perm[1][0] = 1;
-			return perm;
-		}
-		int[][] permutationArray = new int[n][n];
+    /**
+     * Erzeugt ein Array von allen Permutationen von {0,1,2,3,...,n-1}.
+     *
+     * @param n Anzahl Elemente in einer Permutation.
+     * @return Ein Array von Permutationen; jede Zeile ist eine Permutation von {0,1,2,3,...,n-1}.
+     * Permutation ist ein Array von Integern.
+     */
+    public static int[][] permutations(int n) {
+        int[][] data = createArray(n);
+//
+//            if (n % 2 == 1) {
+//                int temp = permutationArray[n][0];
+//                permutationArray[n][0] = permutationArray[n][n - 1];
+//                permutationArray[n][n - 1] = temp;
+//            } else {
+//                int temp = permutationArray[n][n];
+//                permutationArray[n][i] = permutationArray[n][n - 1];
+//                permutationArray[n][n - 1] = temp;
+//            }
+        // TODO
+        return data;
+    }
 
-		for (int i = 0; i < n; i++) {
-			permutations(n - 1);
-
-			if (n % 2 == 1 ){
-				int temp = permutationArray[n][0];
-				permutationArray[n][0] = permutationArray[n][n-1];
-				permutationArray[n][n -1 ] = temp;
-			}
-
-			else {
-				int temp = permutationArray[n][n];
-				permutationArray[n][i] = permutationArray[n][n -1];
-				permutationArray[n][n-1] = temp;
-			}
-		}
-		// TODO
-		return permutationArray;
-	}
-	
-
+    /**
+     * Erzeugt ein zweidimensionales Array mit der dimension [n][n] und füllt alle Werte
+     * Standardmässig ab.
+     * @param n
+     * @return
+     */
+    private static int[][] createArray(int n) {
+        if (n == 1) {
+            int[][] perm = new int[2][1];
+            perm[0][0] = 1;
+            perm[1][0] = 1;
+            return perm;
+        }
+        int[][] permutationArray = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                permutationArray[i][j] = j;
+            }
+        }
+            return permutationArray;
+    }
 }
