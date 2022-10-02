223c7e026fd29c6607a90dd6dbe76b4464e4036f
Achim Grether <a.grether@hotmail.com>
2022-10-02 23:00:48 +0200

    start der implementierung parkettierung

--- Parkettierung/src/ch/ffhs/dua/park/Parkettierung.java
+++ Parkettierung/src/ch/ffhs/dua/park/Parkettierung.java
@@ -1,10 +1,17 @@
 package ch.ffhs.dua.park;
 
+import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Parkettierung {
+
+    static int[] rowCount;
+
+    // Diese HashMap kann zum Caching verwendet werden
+    private static final HashMap<String, BigInteger> CACHE = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println(anzahlParkettierungen(2, 2));
@@ -14,33 +21,58 @@ public class Parkettierung {
     }
 
     public static BigInteger anzahlParkettierungen(int m, int n) {
-        if ((m * n) == 1) return BigInteger.ONE;
+        if ((m * n) == 1) return BigInteger.ZERO;
+        if ((m * n) == 2) return BigInteger.ONE;
 
-        return BigInteger.valueOf(calcPossibilities(m, n));
+        // Generiere ein neues Array und fülle es mit den standardmässigen spalten
+        rowCount = new int[m];
+        Arrays.fill(rowCount, n);
+
+        return BigInteger.valueOf((calcPossibility(rowCount)));
     }
 
-    // Diese HashMap kann zum Caching verwendet werden
-    private static final HashMap<String, BigInteger> CACHE = new HashMap<>();
+    private static int calcPossibility(int[] rowCount) {
 
-    private static int calcPossibilities(int m, int n) {
-        for (int i = 0; i <= m; i++) {
-            int step = 0;
-            while (step <= n) {
-                int temp = 0;
-
-
+        // finde die höchste Anzahl in den spalten
+        int index = 0;
+        int start = rowCount[0];
+        for (int i = 0; i < rowCount.length; i++) {
+            if (rowCount[i] > start) {
+                index = i;
+                start = rowCount[i];
             }
+        }
+        //wähle grösste Zeile aus und setzte dominostein horizontal.
+        int[] rowCopy = new int[rowCount.length];
+        System.arraycopy(rowCount, 0, rowCopy, 0, rowCount.length);
+        rowCopy[index] = rowCopy[index] - 2;
+
+        /**
+         * hier sollte die rekusion starten
+         */
+
+
+        //testen ob noch ein weiterer index verfügbar ist
+        if (rowCount.length > index) {
+
+        // testen ob noch eine gleich lange zeile unterhalb verüfgbar ist
+            if (rowCount[index] <= rowCount[index + 1]) {
+
+                //wähle gröste Zeile aus und setzte dominostein vertical.
+                int[] rowCopy2 = new int[rowCount.length];
+                System.arraycopy(rowCount, 0, rowCopy2, 0, rowCount.length);
+                rowCopy2[index] = rowCopy2[index] - 1;
+                // testen ob noch ein weiterer index frei ist
+                if (rowCopy2.length > index) {
+                    rowCopy2[index + 1] = rowCopy2[index + 1] - 1;
+                }
+            }
+            /**
+             * hier sollte die rekusion starten
+             */
 
         }
+        return 2;
     }
 
-    public static BigInteger factorial(BigInteger number) {
-        BigInteger result = BigInteger.valueOf(1);
-
-        for (long factor = 2; factor <= number.longValue(); factor++) {
-            result = result.multiply(BigInteger.valueOf(factor));
-        }
-
-        return result;
-    }
 }
