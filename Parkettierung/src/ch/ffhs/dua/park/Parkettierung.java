1f46bc90c85f6198778135fabacdc6bbdd03c1c9
Achim Grether <a.grether@hotmail.com>
2022-10-03 20:57:04 +0200

    erste cleanup des codes

--- Parkettierung/src/ch/ffhs/dua/park/Parkettierung.java
+++ Parkettierung/src/ch/ffhs/dua/park/Parkettierung.java
@@ -5,13 +5,6 @@ import java.util.Arrays;
 
 public class Parkettierung {
 
-    public static void main(String[] args) {
-        System.out.println(anzahlParkettierungen(2, 2));
-    }
-
-    private Parkettierung() {
-    }
-
     /**
      * Berechnet die Anzahl möglicher Züge wie ein m x n Schachbrett mit Dominosteinen der Grösse 1 x 2 belegt werden kann.
      * @param m Anzahl der Reihen die das Schachbrett aufweist.
@@ -33,41 +26,39 @@ public class Parkettierung {
         // Zähler für die erfolgreichen (kompletten) Belegungen des Schachbrettes.
         int successfulOccu = 0;
 
-        // finde die höchste Anzahl in den spalten
-        int indexOfLongestRow = 0;
-        int numberOfLongestRow = columnCounter[0];
+        // Finde die höchste Anzahl von Spalten aus allen Reihen.
+        int indexMaxRow = 0;
+        int numMaxRow = columnCounter[0];
         for (int i = 0; i < columnCounter.length; i++) {
-            if (columnCounter[i] > numberOfLongestRow) {
-                indexOfLongestRow = i;
-                numberOfLongestRow = columnCounter[i];
+            if (columnCounter[i] > numMaxRow) {
+                indexMaxRow = i;
+                numMaxRow = columnCounter[i];
             }
         }
 
-        // Wenn alle Felder belegt sind, retourniere den Wert 1 - für eine erfolgreiche Belegung des gesamten Spielfelds.
-        if (numberOfLongestRow == 0) return 1;
+        // Retourniere 1 wenn alle Felder erfolgreich Belegt wurden.
+        if (numMaxRow == 0) return 1;
 
-        //Lege einen Stein Horizontal wenn in dieser Reihe möglich
-        if (numberOfLongestRow >= 2) {
-            // clone das array und fülle es mit den initialen werten ab
+        // Legt einen Stein horizontal in das Spielfend wenn es in der längsten Reihe möglich ist.
+        if (numMaxRow >= 2) {
+            // Kopiere das array und fülle es mit den initialen werten ab.
             int[] newHorizontalRowCounter = new int[columnCounter.length];
             System.arraycopy(columnCounter, 0, newHorizontalRowCounter, 0, columnCounter.length);
             // RowCount um einen horizontalen Stein (2) verringern und sum um eins erhöhen.
-            newHorizontalRowCounter[indexOfLongestRow] = newHorizontalRowCounter[indexOfLongestRow] - 2;
+            newHorizontalRowCounter[indexMaxRow] = newHorizontalRowCounter[indexMaxRow] - 2;
             successfulOccu += placeOneDominoStone(newHorizontalRowCounter);
         }
 
-        //Lege einen Stein vertical wenn in den nächsten beiden Reihen möglich
-        //testen ob noch ein weiterer index verfügbar ist
-        if ((columnCounter.length - 1) > indexOfLongestRow) {
-            // testen ob noch eine gleich lange zeile unterhalb verüfgbar ist
-            if (columnCounter[indexOfLongestRow] <= columnCounter[indexOfLongestRow + 1]) {
-                if (numberOfLongestRow >= 1) {
-                    //wähle gröste Zeile aus und setzte dominostein vertical.
+        // Legt einen Stein vertikal in das Spielfeld wenn es in den nächsten beiden Reihen möglich ist.
+        if ((columnCounter.length - 1) > indexMaxRow) {
+            // Testet ob noch eine gleich lange Zeile unterhalb verüfgbar ist.
+            if (columnCounter[indexMaxRow] <= columnCounter[indexMaxRow + 1]) {
+                if (numMaxRow >= 1) {
+                    // Kopiere das array und fülle es mit den initialen werten ab.
                     int[] rowCopy2 = new int[columnCounter.length];
                     System.arraycopy(columnCounter, 0, rowCopy2, 0, columnCounter.length);
-                    rowCopy2[indexOfLongestRow] = rowCopy2[indexOfLongestRow] - 1;
-                    // testen ob noch ein weiterer index frei ist
-                    rowCopy2[indexOfLongestRow + 1] = rowCopy2[indexOfLongestRow + 1] - 1;
+                    rowCopy2[indexMaxRow] = rowCopy2[indexMaxRow] - 1;
+                    rowCopy2[indexMaxRow + 1] = rowCopy2[indexMaxRow + 1] - 1;
                     successfulOccu += placeOneDominoStone(rowCopy2);
                 }
             }
