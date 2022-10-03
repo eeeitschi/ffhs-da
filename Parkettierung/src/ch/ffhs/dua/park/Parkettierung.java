d30e4998826755f2b53d475bab53821e8b7057d2
Achim Grether <a.grether@hotmail.com>
2022-10-03 19:59:23 +0200

    weitere tests mit der parkettierung - nicht korrekt lauffähig!

--- Parkettierung/src/ch/ffhs/dua/park/Parkettierung.java
+++ Parkettierung/src/ch/ffhs/dua/park/Parkettierung.java
@@ -14,14 +14,14 @@ public class Parkettierung {
     private static final HashMap<String, BigInteger> CACHE = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println(anzahlParkettierungen(2, 2));
+        System.out.println(anzahlParkettierungen(2, 3));
     }
 
     private Parkettierung() {
     }
 
     public static BigInteger anzahlParkettierungen(int m, int n) {
-        if ((m * n) == 1) return BigInteger.ZERO;
+        if ((m * n) <= 2) return BigInteger.ONE;
         if ((m * n) == 2) return BigInteger.ONE;
 
         // Generiere ein neues Array und fülle es mit den standardmässigen spalten
@@ -33,6 +33,8 @@ public class Parkettierung {
 
     private static int calcPossibility(int[] rowCount) {
 
+        int sum = 0;
+
         // finde die höchste Anzahl in den spalten
         int index = 0;
         int start = rowCount[0];
@@ -42,37 +44,46 @@ public class Parkettierung {
                 start = rowCount[i];
             }
         }
-        //wähle grösste Zeile aus und setzte dominostein horizontal.
-        int[] rowCopy = new int[rowCount.length];
-        System.arraycopy(rowCount, 0, rowCopy, 0, rowCount.length);
-        rowCopy[index] = rowCopy[index] - 2;
 
-        /**
-         * hier sollte die rekusion starten
-         */
-
-
-        //testen ob noch ein weiterer index verfügbar ist
-        if (rowCount.length > index) {
-
-        // testen ob noch eine gleich lange zeile unterhalb verüfgbar ist
-            if (rowCount[index] <= rowCount[index + 1]) {
-
-                //wähle gröste Zeile aus und setzte dominostein vertical.
-                int[] rowCopy2 = new int[rowCount.length];
-                System.arraycopy(rowCount, 0, rowCopy2, 0, rowCount.length);
-                rowCopy2[index] = rowCopy2[index] - 1;
-                // testen ob noch ein weiterer index frei ist
-                if (rowCopy2.length > index) {
-                    rowCopy2[index + 1] = rowCopy2[index + 1] - 1;
-                }
-            }
+        //wähle grösste Zeile aus und setzte dominostein horizontal. wenn ein solcher stein noch platz hat.
+        if (start >= 2) {
+            int[] rowCopy = new int[rowCount.length];
+            System.arraycopy(rowCount, 0, rowCopy, 0, rowCount.length);
+            rowCopy[index] = rowCopy[index] - 2;
+            // nur sum +1 wenn nicht der letzte stein ist!
+            sum++;
             /**
              * hier sollte die rekusion starten
              */
-
+            sum += calcPossibility(rowCopy);
         }
-        return 2;
+
+
+        //testen ob noch ein weiterer index verfügbar ist
+        if ((rowCount.length - 1) > index) {
+
+            // testen ob noch eine gleich lange zeile unterhalb verüfgbar ist
+            if (rowCount[index] <= rowCount[index + 1]) {
+
+                if (start >= 1) {
+
+                    //wähle gröste Zeile aus und setzte dominostein vertical.
+                    int[] rowCopy2 = new int[rowCount.length];
+                    System.arraycopy(rowCount, 0, rowCopy2, 0, rowCount.length);
+                    rowCopy2[index] = rowCopy2[index] - 1;
+                    // testen ob noch ein weiterer index frei ist
+                    if ((rowCopy2.length - 1) > index) {
+                        rowCopy2[index + 1] = rowCopy2[index + 1] - 1;
+                        sum++;
+                        /**
+                         * hier sollte die rekusion starten
+                         */
+                        sum += calcPossibility(rowCopy2);
+                    }
+                }
+            }
+        }
+        return sum;
     }
 
 }
