1d8a62a7e6598e26da07972093a48fa72b1e9af6
Achim Grether <a.grether@hotmail.com>
2022-11-01 23:09:29 +0100

    further progress

--- Sortieren/src/ch/ffhs/dua/sort/HeapSort.java
+++ Sortieren/src/ch/ffhs/dua/sort/HeapSort.java
@@ -2,129 +2,138 @@ package ch.ffhs.dua.sort;
 
 import java.util.Arrays;
 
-public class HeapSort
-{
-	/**
-	 * Sortiert ein Array mit Heapsort.
-	 * @param array
-	 */
-	public static void sort(int[] array)
-	{
-		sort(array, 0, array.length);
-	}
-	
-	/**
-	 * Sortiert ein Teilstück eines Array s mit Heapsort.
-	 * @param array
-	 * @param start Index des ersten  Elementes des zu sortierenden Teils.
-	 * @param start Index des letzten Elementes des zu sortierenden Teils.
-	 */
-	public static void sort(int[] array, int start, int end)
-	{
-		//Rearrange array (building heap)
-		for (int i = end / 2 - 1; i >= start; i--) {
-			makeHeap(array, end, i);
-		}
+public class HeapSort {
+    /**
+     * Sortiert ein Array mit Heapsort.
+     *
+     * @param array
+     */
+    public static void sort(int[] array) {
+        sort(array, 0, array.length);
+    }
 
-		//Extract elements from heap one by one
-		for (int i = end - 1; i >= start; i--) {
-			//Current root moved to the end
-			int tmp = array[0];
-			array[0] = array[i];
-			array[i] = tmp;
+    /**
+     * Sortiert ein Teilstück eines Array s mit Heapsort.
+     *
+     * @param array
+     * @param start Index des ersten  Elementes des zu sortierenden Teils.
+     * @param start Index des letzten Elementes des zu sortierenden Teils.
+     */
+    public static void sort(int[] array, int start, int end) {
+        //Rearrange array (building heap)
+        for (int i = end / 2 - 1; i >= start; i--) {
+            makeHeap(array, end, i);
+        }
 
-			makeHeap(array, i, 0);//calling max heapify on the heap reduced
-		}
-	}
-	
-	/**
-	 * Erzeugt aus einem angegebenen Teilstück einen Heap.
-	 * @param array
-	 * @param start Index des ersten Elementes, aus dem ein Heap erzeugt werden sollte.
-	 *              Das ist auch der Index der Wurzel des Heaps; die Kinder der Wurzel
-	 *              liegen dann an den Position start+1 und start+2.
-	 * @param end   Index des letzten Elementes des Stücks, aus dem ein Heap erzeugt werden sollte.
-	 */
-	public static void makeHeap(int[] array, int start, int end)
-	{
-		if(end < 0) return;
-		int max = end; //Initialize max as root
-		int leftChild = 2 * end + 1;
-		int rightChild = 2 * end + 2;
+        //Extract elements from heap one by one
+        for (int i = end - 1; i >= start; i--) {
+            //Current root moved to the end
+            int tmp = array[0];
+            array[0] = array[i];
+            array[i] = tmp;
 
-		//If left child is greater than root
-		if (leftChild < start && array[leftChild] > array[max])
-			max = leftChild;
+            makeHeap(array, i, 0);//calling max heapify on the heap reduced
+        }        // Heap sort
+        for (int i = end; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
 
-		//If right child is greater than max
-		if (rightChild < start && array[rightChild] > array[max])
-			max = rightChild;
+            // Heapify root element
+            sink(array,start, i, 0);
+        }
+    }
 
-		//If max is not root
-		if (max != end) {
-			int swap = array[end];
-			array[end] = array[max];
-			array[max] = swap;
+    /**
+     * Erzeugt aus einem angegebenen Teilstück einen Heap.
+     *
+     * @param array
+     * @param start Index des ersten Elementes, aus dem ein Heap erzeugt werden sollte.
+     *              Das ist auch der Index der Wurzel des Heaps; die Kinder der Wurzel
+     *              liegen dann an den Position start+1 und start+2.
+     * @param end   Index des letzten Elementes des Stücks, aus dem ein Heap erzeugt werden sollte.
+     */
+    public static void makeHeap(int[] array, int start, int end) {
+        for (int i = end / 2; i >= 0; i--) {
+            sink(array, start, end, i);
+        }
 
-			//heapify the affected sub-tree recursively
-			makeHeap(array, start, max);
-		}
-	}
-	
-	/**
-	 * Hilfsmethode für Heapsort:
-	 * Aus einem Teilstück eines Arrays mit den Grenzen start und end
-	 * sollte ein Heap erzeugt werden. Für Indices grösser als index
-	 * sei die Heap-Eigenschaft bereits erfüllt.
-	 * Die Methode ordnet das Stück zwischen index und end so um, 
-	 * dass die Heapeigenschaft für alle Elemente erfüllt ist.
-	 * @param array
-	 * @param start
-	 * @param end
-	 * @param index
-	 */
-	static void sink(int[] array, int start, int end, int index)
-	{
-		// TODO	(Implementieren Sie diese Methode, wenn Sie sie für die Sort-Methoden brauchen.
-	}
-	
-	/**
-	 * Entfernt das Wurzelelement eines Heaps, baut den Heap um,
-	 * so dass er nach dem Entfernen wieder ein Heap ist (mit einem Element weniger), 
-	 * und setzt das ehemalige Wurzelelement an die vormals letzte Stelle im Heap 
-	 * (die nun nicht mehr zum Heap gehört).
-	 * @param array Ein Array, das als Teilstück einen heap enthält.
-	 * @param start Indes der Wurzel des heaps
-	 * @param end   Index des letzten Heap-Elements.
-	 */
-	public static void removeHeapRoot(int[] array, int start, int end)
-	{
-		// TODO	(Implementieren Sie diese Methode, wenn Sie sie für die Sort-Methoden brauchen.
-	}
-	
-	/**
-	 * Berechnet den Index des linken Kindelementes in einem Heap.
-	 * @param parentIndex
-	 * @param offset Offset für Heap-Eigenschaft: entspricht 
-	 *         dem Index der Heapwurzel - 1
-	 * @return Index des linken Kindes
-	 */
-	static int leftChild(int parentIndex, int offset)
-	{
-		return 2 * parentIndex - offset;
-	}
+    }
 
-	/**
-	 * Berechnet den Index des rechten Kindelementes in einem Heap.
-	 * @param parentIndex
-	 * @param offset Offset für Heap-Eigenschaft: entspricht 
-	 *         dem Index der Heapwurzel - 1
-	 * @return Index des rechten Kindes
-	 */
-	static int rightChild(int parentIndex, int offset)
-	{
-		return leftChild(parentIndex, offset) + 1;
-	}
+    /**
+     * Hilfsmethode für Heapsort:
+     * Aus einem Teilstück eines Arrays mit den Grenzen start und end
+     * sollte ein Heap erzeugt werden. Für Indices grösser als index
+     * sei die Heap-Eigenschaft bereits erfüllt.
+     * Die Methode ordnet das Stück zwischen index und end so um,
+     * dass die Heapeigenschaft für alle Elemente erfüllt ist.
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @param index
+     */
+    static void sink(int[] array, int start, int end, int index) {
+        // Find largest among root, left child and right child
+        // Find largest among root, left child and right child
+        int largest = index;
+        int l = 2 * index + 1;
+        int r = 2 * index + 2;
+
+        if (l <= end && array[l] > array[largest])
+            largest = l;
+
+        if (r <= end && array[r] > array[largest])
+            largest = r;
+
+        // Swap and continue heapifying if root is not largest
+        if (largest != index) {
+            int swap = array[index];
+            array[index] = array[largest];
+            array[largest] = swap;
+
+            sink(array, start, end, largest);
+        }
+        // TODO	(Implementieren Sie diese Methode, wenn Sie sie für die Sort-Methoden brauchen.
+    }
+
+    /**
+     * Entfernt das Wurzelelement eines Heaps, baut den Heap um,
+     * so dass er nach dem Entfernen wieder ein Heap ist (mit einem Element weniger),
+     * und setzt das ehemalige Wurzelelement an die vormals letzte Stelle im Heap
+     * (die nun nicht mehr zum Heap gehört).
+     *
+     * @param array Ein Array, das als Teilstück einen heap enthält.
+     * @param start Indes der Wurzel des heaps
+     * @param end   Index des letzten Heap-Elements.
+     */
+    public static void removeHeapRoot(int[] array, int start, int end) {
+        // TODO	(Implementieren Sie diese Methode, wenn Sie sie für die Sort-Methoden brauchen.
+    }
+
+    /**
+     * Berechnet den Index des linken Kindelementes in einem Heap.
+     *
+     * @param parentIndex
+     * @param offset      Offset für Heap-Eigenschaft: entspricht
+     *                    dem Index der Heapwurzel - 1
+     * @return Index des linken Kindes
+     */
+    static int leftChild(int parentIndex, int offset) {
+        return 2 * parentIndex - offset;
+    }
+
+    /**
+     * Berechnet den Index des rechten Kindelementes in einem Heap.
+     *
+     * @param parentIndex
+     * @param offset      Offset für Heap-Eigenschaft: entspricht
+     *                    dem Index der Heapwurzel - 1
+     * @return Index des rechten Kindes
+     */
+    static int rightChild(int parentIndex, int offset) {
+        return leftChild(parentIndex, offset) + 1;
+    }
 
 
 }
