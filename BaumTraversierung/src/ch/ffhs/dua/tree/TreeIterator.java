273b72fc82870501533436ed5af513d68258d5f6
Achim Grether <a.grether@hotmail.com>
2022-12-02 23:33:17 +0100

    treeiterator finished

--- BaumTraversierung/src/ch/ffhs/dua/tree/TreeIterator.java
+++ BaumTraversierung/src/ch/ffhs/dua/tree/TreeIterator.java
@@ -1,36 +1,50 @@
 package ch.ffhs.dua.tree;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Stack;
 
 /**
- * Ein Iterator, der in Depth-First Reihenfolge alle Werte 
+ * Ein Iterator, der in Depth-First Reihenfolge alle Werte
  * der Knoten eines Baumes ausgibt.
  *
  * @param <N> Typ des Knotenwertes.
  */
-public class TreeIterator<N> implements Iterator<N>
-{
-	/** 
-	 * Erzeugt einen neuen neuen Baum-Knoten-Iterator
-	 * @param node Die Wurzel des zu traversierenden Baumes.
-	 */
-	public TreeIterator(TreeNode<N> node)
-	{
-		// TODO
-	}
+public class TreeIterator<N> implements Iterator<N> {
+    private int nextIndex = 0;
+    ArrayList<N> orderedList = new ArrayList<>();
 
-	@Override
-	public boolean hasNext() 
-	{
-		//TODO
-		return false;
-	}
+    /**
+     * Erzeugt einen neuen neuen Baum-Knoten-Iterator
+     *
+     * @param node Die Wurzel des zu traversierenden Baumes.
+     */
+    public TreeIterator(TreeNode<N> node) {
+        Stack<TreeNode<N>> stack = new Stack<>();
+        stack.push(node);
 
-	@Override
-	public N next() {
-		// TODO
-		return null;
-	}
-	
-	// remove() muss nicht implementiert werden.
+        while (!stack.isEmpty()) {
+            TreeNode<N> temp = stack.peek();
+            stack.pop();
+
+            // Speichere den Wert als besuchter Knoten
+            orderedList.add(temp.value());
+            // Füge alle ChildNodes auf den Stack (von links nach rechts).
+            for (int i = temp.children().size() - 1; i >= 0; i--) {
+                stack.push(temp.children().get(i));
+            }
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextIndex <= orderedList.size() - 1;
+    }
+
+    @Override
+    public N next() {
+        return orderedList.get(nextIndex++);
+    }
+
+    // remove() muss nicht implementiert werden.
 }
