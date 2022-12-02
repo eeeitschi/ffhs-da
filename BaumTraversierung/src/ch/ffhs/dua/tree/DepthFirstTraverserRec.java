2d437968a79562429f2610730099118fcfa5bb8c
Achim Grether <a.grether@hotmail.com>
2022-12-02 22:43:39 +0100

    depthfirsttraverserrec finished

--- BaumTraversierung/src/ch/ffhs/dua/tree/DepthFirstTraverserRec.java
+++ BaumTraversierung/src/ch/ffhs/dua/tree/DepthFirstTraverserRec.java
@@ -6,27 +6,36 @@ package ch.ffhs.dua.tree;
  *
  * @param <N>
  */
-public abstract class DepthFirstTraverserRec<N> 
-{
-	/**
-	 * Traversiert einen Baum mit DepthFirst Strategie.
-	 * @param node Die Wurzel des zu traversierenden Baumes.
-	 */
-	public void traverse(TreeNode<N> node) 
-	{
-		// TODO
-	}
-	
-	/**
-	 * Operation auf einem Knoten, bevor die Nachkommen besucht wurden.
-	 * @param value
-	 */
-	abstract protected void preOperation(N value);   
-    
-	/**
-	 * Operation auf einem Knoten, nachdem die Nachkommen besucht wurden.
-	 * @param value
-	 */
-	abstract protected void postOperation(N value);  
+public abstract class DepthFirstTraverserRec<N> {
+    /**
+     * Traversiert einen Baum mit DepthFirst Strategie.
+     *
+     * @param node Die Wurzel des zu traversierenden Baumes.
+     */
+    public void traverse(TreeNode<N> node) {
+            // PreOperation Aufruf
+            preOperation(node.value());
+
+            // Traversiere durch alle ChildNodes und starte rekursiven Aufruf.
+            for (TreeNode<N> child : node.children()) {
+                traverse(child);
+            }
+            // PostOperation Aufruf
+            postOperation(node.value());
+    }
+
+    /**
+     * Operation auf einem Knoten, bevor die Nachkommen besucht wurden.
+     *
+     * @param value
+     */
+    abstract protected void preOperation(N value);
+
+    /**
+     * Operation auf einem Knoten, nachdem die Nachkommen besucht wurden.
+     *
+     * @param value
+     */
+    abstract protected void postOperation(N value);
 
 }   
